613a4e46486c02a4f6bbc221c2073f69ac7ffad7
noaddress <no.address.0@gmail.com>
2013-10-04 23:40:39 +0200

    h

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-print("user: ".$_GET["user"]);
-print("<br>");
-print("text: ".$_GET["text"]);
- $file=fopen($_GET["user"].".txt","a") or exit("Unable to write file!");
- fwrite($file,"%26");
- fwrite($file,$_GET["text"]);
- fclose($file);
-?>
